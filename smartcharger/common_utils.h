2197beeff36d2819d54231ff3cf72cbbadb00d71
bastoon577-lang <bastoon577@gmail.com>
2026-08-31 16:22:28 +0200

    FEAT : Bump version into v1.0.2 for good harmony between Git Repo

--- smartcharger/common_utils.h
+++ smartcharger/common_utils.h
@@ -6,7 +6,7 @@
 #include "../../hal_utils.h"
 
 //< MACRO de version logicielle
-#define V_LOGICIEL                "v1.02"               // Version Logicielle
+#define V_LOGICIEL                "v1.0.2"              // Version Logicielle
 
 //< MACRO du Mode Access Point
 #define AP_SSID                   "SmartCharger"        // SSID SmartCharger Access Point
